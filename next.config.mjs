06bae6b86859e52ce6a54b69ccec292bb21c9a63
favazmk12-creator <favazmk12@gmail.com>
2026-06-29 19:40:10 +0530

    chore: initialize Next.js 14 project

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
